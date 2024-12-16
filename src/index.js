aae8a685444e0ad6ae47525c610eb9c8b79bc397
Wissididom <30803034+Wissididom@users.noreply.github.com>
2024-12-16 10:22:13 +0000

    Initial authorization code grant flow implementation

--- src/index.js
+++ src/index.js
@@ -111,7 +111,93 @@ app.use(
   }),
 );
 
-app.get("/", (req, res) => res.send("Twitch EventSub Webhook Endpoint"));
+app.get("/", (req, res) => {
+  res.send("buttsbot");
+});
+
+function redirect(res, clientId, redirectUri, scopes) {
+  res.redirect(`https://id.twitch.tv/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${encodeURIComponent(scopes.join(' '))}`);
+}
+
+app.get("/streamer-auth", (req, res) => {
+  redirect(res, process.env.TWITCH_CLIENT_ID, 'TODO', [
+    "channel:bot"
+  ]);
+});
+
+app.get("/chatter-auth", (req, res) => {
+  redirect(res, process.env.TWITCH_CLIENT_ID, 'TODO', [
+    "user:read:chat",
+    "user:write:chat",
+    "user:bot"
+  ]);
+});
+
+app.get("/streamer-and-chatter-auth", (req, res) => {
+  redirect(res, process.env.TWITCH_CLIENT_ID, 'TODO', [
+    "channel:bot",
+    "user:read:chat",
+    "user:write:chat",
+    "user:bot"
+  ]);
+});
+
+function getForWhom(scopes) {
+  if (scopes.includes('channel:bot')) {
+    if (scopes.includes('user:read:chat') && scopes.includes('user:write:chat') && scopes.includes('user:bot')) {
+      return "Streamer and Chatter";
+    } else {
+      return "Streamer";
+    }
+  } else {
+    if (scopes.includes('user:read:chat') && scopes.includes('user:write:chat') && scopes.includes('user:bot')) {
+      return "Chatter";
+    } else {
+      return "N/A";
+    }
+  }
+}
+
+app.get("/auth-callback", async (req, res) => {
+  res.setHeader('content-type', 'text/plain');
+  if (req.query.code) {
+    const authCode = req.query.code;
+    const fetchResponse = await fetch('https://id.twitch.tv/oauth2/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: new URLSearchParams({
+        'client_id': process.env.TWITCH_CLIENT_ID,
+        'client_secret': process.env.TWITCH_CLIENT_SECRET,
+        'code': authCode,
+        'grant_type': 'authorization_code',
+        'redirect_uri': 'TODO'
+      })
+    });
+    if (fetchResponse.ok) {
+      const json = fetchResponse.json();
+      const accessToken = json.access_token;
+      const user = getUser(null, process.env.TWITCH_CLIENT_ID, accessToken);
+      const forWhom = getForWhom(json.scope);
+      if (user.display_name.toLowerCase() == user.login) {
+        res.send(`Got Tokens for ${forWhom} ${user.display_name}`);
+      } else {
+        res.send(`Got Tokens for ${forWhom} ${user.display_name} (${user.login})`);
+      }
+    } else {
+      res.send(fetchResponse.text());
+    }
+  } else if (req.query.error) {
+    if (req.query.error_description) {
+      res.send(`The following error occured:\n${req.query.error}\n${req.query.error_description}`);
+    } else {
+      res.send(`The following error occured:\n${req.query.error}`);
+    }
+  } else {
+    res.send("This endpoint is intended to be redirected from Twitch's auth flow. It is not meant to be called directly");
+  }
+});
 
 app.post("/", async (req, res) => {
   let secret = process.env.EVENTSUB_SECRET;
